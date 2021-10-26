c80447dc031d38c797dc56f751c61897ffaa6115
Leonardo Ramos <leorrc96@gmail.com>
2021-10-26 17:25:17 -0400

    update(app.js): se usa la libreria cors

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
 const express = require("express");
 const path = require("path");
-const app = express();
+const cors = require('cors');
 const { statusLogin } = require('./whatsapp/whatsapp')
 const whatsAppRoutes = require("./routes/whatsapp.route");
 const serverControlRoutes = require("./routes/server-control.route");
 
-const fs = require('fs').promises;
+const app = express();
 const port = process.env.PORT || 3003;
-
 
 /* fs.unlink('user.json').catch(()=>{}) */
 
 //CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+app.use(cors('*'));
 
 app.set("views", path.join(__dirname, "views"));
 // serving static files
